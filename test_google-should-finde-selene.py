430914fa0dea30c56b5e4c6f78c604f556fbc37b
Smillyhamster <smillyhamster@gmail.com>
2022-12-29 20:53:03 +0300

    change requirements

--- test_google-should-finde-selene.py
+++ test_google-should-finde-selene.py
@@ -1,8 +1,13 @@
 from selene.support.shared import browser
 from selene import be, have
+
+
 def test_google_search(open_browser):
     browser.element('[name="q"]').should(be.blank).type('selene').press_enter()
-    browser.element('#search').should(have.text('selene: User-oriented Web UI browser tests in Python'))
+    browser.element('#search').should(have.text('yashaka/selene: User-oriented Web UI browser tests in Python'))
+
+
 def test_google_search_bad_request(open_browser):
-    browser.element('[name="q"]').should(be.blank).type('Селен - это круто').press_enter()
-    browser.element('#res').should(have.text('По запросу "Селен - это круто" ничего не найдено.'))
+    browser.element('[name="q"]').should(be.blank).type('S_eLe21h1223gjdjfhg34345234jgjhfdj').press_enter()
+    browser.element('[id="topstuff"]').should(have.text('ничего не найдено'))
+
