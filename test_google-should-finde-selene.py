fdeebd8823c88f109d21991bd55a7da7a122afc3
Smillyhamster <smillyhamster@gmail.com>
2022-12-29 09:36:08 +0300

    add new tests

--- test_google-should-finde-selene.py
+++ test_google-should-finde-selene.py
@@ -1,6 +1,8 @@
 from selene.support.shared import browser
-from selene import have, be
-
-browser.open('https://google.com/ncr')
-browser.element('[name=q').should(be.blank).type('selene').press_enter()
-browser.element('[id=search').should(have.text('selene: User-oriented Web UI browser tests'))
+from selene import be, have
+def test_google_search(open_browser):
+    browser.element('[name="q"]').should(be.blank).type('selene').press_enter()
+    browser.element('#search').should(have.text('selene: User-oriented Web UI browser tests in Python'))
+def test_google_search_bad_request(open_browser):
+    browser.element('[name="q"]').should(be.blank).type('Селен - это круто').press_enter()
+    browser.element('#res').should(have.text('По запросу "Селен - это круто" ничего не найдено.'))
