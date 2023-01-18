5b1c22b90ffb404247ba65582e01cf020e10c786
Smillyhamster <smillyhamster@gmail.com>
2023-01-18 10:49:08 +0300

    Поправил тесты, теперь проходят

--- conftest.py
+++ conftest.py
@@ -1,32 +1,20 @@
 import pytest
 from selene.support.shared import browser
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
 
 
 @pytest.fixture()
-def config_chrome():
-    options = Options()
-    # options.add_argument('chrome')  # Use headless if you do not need a browser UI
-    # options.add_argument('--start-maximized')
-    options.add_argument('--window-size=1920,1080')
-    # options.add_experimental_option('excludeSwitches', ['enable-logging'])
-    return options
-
-# Analog
-# @pytest.fixture(scope="class")
-# def open_browser():
-#     browser.config.window_height = 950
-#     browser.config._window_width = 1600
-
-
-@pytest.fixture()
-def web_driver_config(config_chrome):
-    browser.config.driver = webdriver.Chrome(options=config_chrome)
+def set_webdriver():
+    browser.config.driver = webdriver.Chrome(ChromeDriverManager().install())
     return browser.config.driver
 
 
 @pytest.fixture()
-def open_browser(web_driver_config):
-    browser.open('https://google.com')
-    yield
+def set_browser_max_size():
+    browser.config.driver.maximize_window()
+
+
+@pytest.fixture()
+def open_browser(set_webdriver, set_browser_max_size):
+    browser.open('https://google.com/ncr')
--- test_google-should-finde-selene.py
+++ test_google-should-finde-selene.py
@@ -1,13 +1,13 @@
 from selene.support.shared import browser
-from selene import be, have
+from selene import have, be
 
 
-def test_google_search(open_browser):
-    browser.element('[name="q"]').should(be.blank).type('selene').press_enter()
-    browser.element('#search').should(have.text('yashaka/selene: User-oriented Web UI browser tests in Python'))
+def test_google_good_search(open_browser):
+    browser.element('[name=q]').should(be.blank).type('selene').press_enter()
+    browser.element('[id=search]').should(have.text('yashaka/selene: User-oriented Web UI browser tests'))
 
 
-def test_google_search_bad_request(open_browser):
-    browser.element('[name="q"]').should(be.blank).type('S_eLe21h1223gjdjfhg34345234jgjhfdj').press_enter()
-    browser.element('[id="topstuff"]').should(have.text('ничего не найдено'))
-
+def test_google_bad_search(open_browser):
+    browser.element('[name=q]').should(be.blank).type('sdfg5245tfg2452134sgsdfg').press_enter()
+    browser.element('[id=topstuff]').should(
+        have.text('Your search - sdfg5245tfg2452134sgsdfg - did not match any documents.'))
